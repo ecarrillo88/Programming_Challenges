b8b809a4fe14f64c26617f63a6602d230265f115
Enrique Carrillo <enriquecarrillo88@gmail.com>
2014-02-11 12:45:07 +0100

    PC: Wrong Answer / UVA: Accepted.

--- src/tema04/StacksOfFlapjacks.java
+++ src/tema04/StacksOfFlapjacks.java
@@ -1,18 +1,20 @@
 package tema04;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StacksOfFlapjacks {
 	private static int[] stack;
+	private static ArrayList<Integer> volteos;
 
 	// Algoritmo basado en SELECTION SORT O(n^2)
-	
-	private static void voltearTortitas() {
+	private static void ordenarTortitas() {
+		// Indica hasta que posicion estan ordenadas las tortitas
 		int fin = stack.length - 1;
 		while (!estaOrdenado()) {
 			int max = 0;
 			int posMax = 0;
-			for (int i = 1; i < fin; i++) {
+			for (int i = 1; i <= fin; i++) {
 				if (stack[i] > max) {
 					max = stack[i];
 					posMax = i;
@@ -26,6 +28,13 @@ public class StacksOfFlapjacks {
 
 	private static void volteo(int fin) {
 		int numCambios = (fin / 2);
+		if (numCambios != 0) {
+			// La torre de tortitas esta al reves ya que el primer elemento en
+			// realidad es el ultimo en la torre y al guardar la posicion de
+			// volteo hay que calcular la posicion invertida en el array
+			volteos.add((stack.length - 1 - fin) + 1);
+		}
+
 		int aux, i = 1, j = fin;
 		while (numCambios > 0) {
 			aux = stack[i];
@@ -46,25 +55,33 @@ public class StacksOfFlapjacks {
 		return true;
 	}
 
-	/*
-	 * Pruebas 1 5 2 3 4 | 4 1 2 0 || 5 2 1 4 3 6 | 1 1 2 5 3 4 4 5 0
-	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
 		while (scan.hasNextLine()) {
-			// Mediante split almacenamos la linea en un array auxiliar
-			String[] aux = scan.nextLine().split(" ");
+			volteos = new ArrayList<Integer>();
 
+			// Mediante split almacenamos la linea en un array auxiliar
+			String original = scan.nextLine();
+			String[] aux = original.split(" ");
+
+			// La posicion 0 no se va ha utilizar
 			stack = new int[aux.length + 1];
 
-			// Convertimos cada elemento del array auxiliar a Integer
+			// Convertimos cada elemento del array auxiliar a Integer y lo
+			// guardamos en stack
 			for (int i = 1; i < stack.length; i++) {
 				stack[i] = Integer.parseInt(aux[i - 1]);
 			}
 
-			voltearTortitas();
-			System.out.println("yaaaa");
+			ordenarTortitas();
+
+			// Imprimimos por pantalla los resultados
+			System.out.println(original);
+			for (int i = 0; i < volteos.size(); i++) {
+				System.out.print(volteos.get(i) + " ");
+			}
+			System.out.println("0");
 		}
 
 		scan.close();
